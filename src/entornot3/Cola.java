c4a0c70f8d1f7bb856892acc12c3962de04fa811
david <dcs0004@alu.medac.es>
2022-03-30 14:10:23 +0200

    version 1.1

---
+++ src/entornot3/Cola.java
@@ -0,0 +1,23 @@
+
+
+package entornot3;
+
+import java.util.ArrayList;
+
+
+public class Cola {
+    ArrayList <Cliente> Lista_Cola = new ArrayList<Cliente>();
+    
+    
+    
+    
+    public void dejar_pasar(Cliente pasar){
+       if( Lista_Cola.contains(pasar) ){
+           Lista_Cola.add(1 , pasar);
+       
+       }
+    
+    }
+    
+    
+}
