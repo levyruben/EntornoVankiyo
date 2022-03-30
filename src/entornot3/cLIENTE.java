54cb23dc3bfc0083dbb12987ab906dbfb05624b0
Your Name <fvg0001@alu.medac.es>
2022-03-30 14:02:54 +0200

    clientes_v1

---
+++ src/entornot3/cLIENTE.java
@@ -0,0 +1,48 @@
+package entorno;
+
+public class cLIENTE {
+
+    String nombre;
+    double gasta;
+    int gastoTOT;
+    String listaProductos[] = {"Manzanas", "Peras", "Melon", "sandia", "fresas", "platanos"};
+double precio;
+    public cLIENTE(String nom, String prct) {
+        String nombre = nom;
+        String listaProductos = prct;
+
+        if (prct == "Manzanas") {
+            precio=2;
+            gasta+=precio;
+        }
+       if (prct == "Peras") {
+            precio=5;
+            gasta+=precio;
+        }
+       if (prct == "Melon") {
+            precio=16;
+            gasta+=precio;
+        }
+       if (prct == "sandia") {
+            precio=23;
+            gasta+=precio;
+        }
+       if (prct == "fresas") {
+            precio=30;
+            gasta+=precio;
+        }
+       if (prct == "platanos") {
+            precio=7;
+            gasta+=precio;
+        }
+       this.gastoTOT+=gasta;
+    }
+   public String toString(){
+       String res="";
+       res+="El cliente se llama: "+this.nombre;
+       res+="El cliente gasta: "+this.gastoTOT;
+        return res;
+       
+   } 
+   
+}
