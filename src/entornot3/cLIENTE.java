359368515d5150c34817026aedcc647bab937d14
david <dcs0004@alu.medac.es>
2022-04-06 12:25:20 +0200

    cambios clase

--- src/entornot3/cLIENTE.java
+++ src/entornot3/cLIENTE.java
@@ -1,48 +1,53 @@
-package entorno;
+package entornot3;
 
-public class Cliente {
 
-    String nombre;
-    double gasta;
-    int gastoTOT;
-    String listaProductos[] = {"Manzanas", "Peras", "Melon", "sandia", "fresas", "platanos"};
-double precio;
-    public Cliente(String nom, String prct) {
-        String nombre = nom;
-        String listaProductos = prct;
+    public class Cliente {
 
-        if (prct == "Manzanas") {
-            precio=2;
-            gasta+=precio;
+        String nombre;
+        double gasta;
+        int gastoTOT;
+        String listaProductos[] = {"Manzanas", "Peras", "Melon", "sandia", "fresas", "platanos"};
+        double precio;
+
+        public Cliente(String nom, String prct) {
+            String nombre = nom;
+            String listaProductos = prct;
+
+            if (prct == "Manzanas") {
+                precio = 2;
+                gasta += precio;
+            }
+            if (prct == "Peras") {
+                precio = 5;
+                gasta += precio;
+            }
+            if (prct == "Melon") {
+                precio = 16;
+                gasta += precio;
+            }
+            if (prct == "sandia") {
+                precio = 23;
+                gasta += precio;
+            }
+            if (prct == "fresas") {
+                precio = 30;
+                gasta += precio;
+            }
+            if (prct == "platanos") {
+                precio = 7;
+                gasta += precio;
+            }
+            this.gastoTOT += gasta;
         }
-       if (prct == "Peras") {
-            precio=5;
-            gasta+=precio;
+
+        public String toString() {
+            String res = "";
+            res += "El cliente se llama: " + this.nombre;
+            res += "El cliente gasta: " + this.gastoTOT;
+            return res;
+
         }
-       if (prct == "Melon") {
-            precio=16;
-            gasta+=precio;
-        }
-       if (prct == "sandia") {
-            precio=23;
-            gasta+=precio;
-        }
-       if (prct == "fresas") {
-            precio=30;
-            gasta+=precio;
-        }
-       if (prct == "platanos") {
-            precio=7;
-            gasta+=precio;
-        }
-       this.gastoTOT+=gasta;
+
     }
-   public String toString(){
-       String res="";
-       res+="El cliente se llama: "+this.nombre;
-       res+="El cliente gasta: "+this.gastoTOT;
-        return res;
-       
-   } 
-   
-}
+
+
